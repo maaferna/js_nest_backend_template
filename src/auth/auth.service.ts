23182046ef05a211c22f85f13d572af8834e0495
mparraf <maaferna@gmail.com>
2023-11-07 19:03:18 -0300

    It was handled the 500 error when POST with repeat email in the database

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
-import { Injectable, Post } from '@nestjs/common';
+import { ForbiddenException, Injectable, Post } from '@nestjs/common';
 import { DatabaseConnectionService } from 'src/database_connection/database_connection.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { error } from 'console';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
@@ -10,27 +12,36 @@ export class AuthService {
   async signup(dto: AuthDto) {
     // Generate the password hash
     const hash = await argon.hash(dto.password);
-    const user = await this.prisma.user.create({
-      data: {
-        username: dto.username,
-        email: dto.email,
-        hash,
-        profile: dto.profile,
-        createdAt: dto.createdAt,
-        updatedAt: dto.updatedAt,
-      },
-      select: {
-        id: true,
-        email: true,
-        username: true,
-        createdAt: true,
-      },
-    });
-
     // Save the new user in the database
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          username: dto.username,
+          email: dto.email,
+          hash,
+          profile: dto.profile,
+          createdAt: dto.createdAt,
+          updatedAt: dto.updatedAt,
+        },
+        select: {
+          id: true,
+          email: true,
+          username: true,
+          createdAt: true,
+        },
+      });
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
 
     // Return the new user
-    return user;
   }
 
   signin() {
