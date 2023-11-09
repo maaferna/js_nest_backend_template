1eb071b72357fb3712604610be4c759e9040737b
mparraf <maaferna@gmail.com>
2023-11-09 18:40:07 -0300

    It was created a customize GetUser class to use how decorator in the user.controller from auth/get-userd.decorators

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,16 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
 import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
+@UseGuards(JwtGuard)
 export class UserController {
-  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
+  getMe(@GetUser() user: User) {
     // console.log({ user: req.user});
-    return req.user;
+    return user;
   }
 }
