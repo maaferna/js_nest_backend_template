66b55cbc096bf21d1e62e9e6c9f89404c14aef0b
mparraf <maaferna@gmail.com>
2023-11-09 18:15:31 -0300

    It was customized the JwtGuard to handle the token, in the auth directory it's was created a new folder named guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get('me')
   getMe(@Req() req: Request) {
     // console.log({ user: req.user});
